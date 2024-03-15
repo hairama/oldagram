8ce6bbc01c065ff05297882f80eda80576d156d3
hairama <67448768+hairama@users.noreply.github.com>
2024-03-15 12:47:38 -0700

    added modal
    with comments and styling

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { v4 as uuidv4 } from 'https://jspm.dev/uuid'
 const postFeed = document.getElementById("post-holder")
 const addPostBtn = document.getElementById("add-post-btn")
 const fileInput = document.getElementById("file-input")
+const overlay = document.getElementById('overlay')
 
 
 
@@ -16,11 +17,14 @@ document.addEventListener("click", function(e) {
     if(e.target.dataset.like){
         handleLikeClick(e.target.dataset.like)
     }
-    if(e.target.dataset.comment){
-        console.log(e.target.dataset.comment)
+    if(e.target.dataset.commentBtn){
+        console.log(e.target.dataset.commentBtn)
     }
     if(e.target.dataset.dm){
         console.log(e.target.dataset.dm)
+    }
+    if(e.target.dataset.viewPostComments) {
+        openCommentModal(e.target.dataset.viewPostComments)
     }
 
 })
@@ -47,12 +51,20 @@ function getFeed(postData) {
     
     postData.map((aSinglePost) => { 
         
-        const {name, username, location, avatar, post, comment, likes, uuid, isLiked} = aSinglePost
+        const {name, username, location, avatar, post, postText, likes, replies, uuid, isLiked} = aSinglePost
         
         let likeImgSrc = 'images/icon-heart.png'
-        if (isLiked){
-            likeImgSrc = "images/icon-heart-red.png"
-        }
+            if (isLiked){
+                likeImgSrc = "images/icon-heart-red.png"
+            }
+
+        let repliesPreviewHtml = ''
+            if (replies.length > 1) {
+                repliesPreviewHtml = `View all ${replies.length} comments`
+            } else if (replies.length === 1) {
+                repliesPreviewHtml = `View 1 comment`
+            }
+
 
         postFeedHTML.push(`<div> 
                     <section class="post"> <!-- CONTAINS AN INSTANCE OF A POST -->
@@ -67,12 +79,15 @@ function getFeed(postData) {
                             <div class="post-footer"> <!-- CONTAINS THE FOOTER OF A POST -->
                                 <div class="icon-container">
                                     <img class="icon" data-like="${uuid}" src="${likeImgSrc}" >
-                                    <img class="icon" data-comment="${uuid}"  src="images/icon-comment.png">
+                                    <img class="icon" data-comment-btn="${uuid}"  src="images/icon-comment.png">
                                     <img class="icon" data-dm="${uuid}" src="images/icon-dm.png">
                                 </div>
-                                <p class="bold-text">${likes} likes</p>
+                                <p class="bold-text like-count">${likes} likes</p>
                                 <div>
-                                    <p class="comment"><span class="bold-text">${username}</span> ${comment}</p>
+                                    <p class="post-text"><span class="bold-text">${username}</span> ${postText}</p>
+                                    <div id="reply-preview"-${uuid}" class="reply-preview">
+                                        <p data-view-post-comments="${uuid}">${repliesPreviewHtml}</p>
+                                    </div>
                                 </div>
                             </div> <!-- END OF POST-FOOTER -->
                         </section> <!--END OF POST SECTION -->  
@@ -94,7 +109,63 @@ function handleLikeClick(postID){
     })
 }        
         
- 
+function openCommentModal(postID){
+    document.getElementById("comments-modal").style.display = "flex"
+    
+    document.getElementById("comments-modal-close-btn").addEventListener("click", function(){
+        document.getElementById("comments-modal").style.display = "none"
+        document.body.style.overflow = ''
+        overlay.style.display = 'none'
+    })
+
+    document.body.style.overflow = 'hidden'
+    overlay.style.display = 'block'
+
+    
+    
+    let commentModalHTML = []
+        
+        posts.forEach(function(post) {
+            if (post.uuid === postID) {
+                
+                commentModalHTML = post.replies.map(function(reply) {
+        
+                    return  `
+                                <div class="post-reply">
+                                <div class="reply-inner">
+                                    <!-- <img src="${reply.profilePic}" class="profile-pic"> -->
+                                        <div>
+                                            <p class="handle">${reply.handle}</p>
+                                            <p class="reply-text">${reply.replyText}</p>
+                                        </div>
+                                    </div>
+                            </div>
+                            `  
+                }).join('')
+            }
+        
+        // `
+        // <div class="reply-inner">
+        //     <p>${reply.replyText}</p>
+        // </div>` 
+
+    document.getElementById("comments-modal-inner").innerHTML = commentModalHTML
+    })
+
+    
+    /*
+        locate the post with postID in the posts array
+        iterate through the replies array and generate html to render the comment modal
+        unhide the comment modal
+        
+        on the comment modal
+        display a list of comments
+        user can scroll through comments
+        
+        user can add a comment
+        user can close the modal
+    */
+}       
 
 
 getFeed(posts)
--- posts.js
+++ posts.js
@@ -5,10 +5,23 @@ export const posts = [
         location: "Zundert, Netherlands",
         avatar: "images/avatar-vangogh.jpg",
         post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
+        postText: "just took a few mushrooms lol",
         likes: 21,
         uuid: "5135c61d-7e4e-40de-b9f5-170ed23989c4",
+        replies: [
+            {
+                handle: `@StackOverflower ☣️`,
+                profilePic: `images/overflow.png`,
+                replyText: `No. `,
+            },
+            {
+                handle: `@StackOverflower ☣️`,
+                profilePic: `images/overflow.png`,
+                replyText: `Obviosuly not.`,
+            }
+        ],
         isLiked: false
+        
     },
     {
         name: "Gustave Courbet",
@@ -16,9 +29,10 @@ export const posts = [
         location: "Ornans, France",
         avatar: "images/avatar-courbet.jpg",
         post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed tbh",
+        postText: "i'm feelin a bit stressed tbh",
         likes: 4,
         uuid: "af9019ee-4ed5-4a6e-89c2-a3d478b62179",
+        replies: [],
         isLiked: false
     },
         {
@@ -27,9 +41,10 @@ export const posts = [
         location: "Paris, France",
         avatar: "images/avatar-ducreux.jpg",
         post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        postText: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
         likes: 152,
         uuid: "22720426-9819-4874-8b77-92c0b732b150",
+        replies: [],
         isLiked: false
     },
         {
@@ -38,9 +53,31 @@ export const posts = [
         location: "Olso, Norway",
         avatar: "images/avatar-borgen.png",
         post: "images/post-borgen.jpg",
-        comment: `Ever notice how ppl who say "AI will take our jobs" only have 1 string on their fiddle and no skin? Ignore them!`,
+        postText: `Ever notice how ppl who say "AI will take our jobs" only have 1 string on their fiddle and no skin? Ignore them!`,
         likes: 2001,
         uuid: "a2909207-c30a-4dc3-aec4-87ef06ce6d55",
+        replies: [
+            {
+                handle: `@StackOverflower ☣️`,
+                profilePic: `images/overflow.png`,
+                replyText: `Go.`,
+            },
+            {
+                handle: `@StackOverflower ☣️`,
+                profilePic: `images/overflow.png`,
+                replyText: `get a job`,
+            },
+            {
+                handle: `@StackOverflower ☣️`,
+                profilePic: `images/overflow.png`,
+                replyText: `Obviosuly get McDonald's.`,
+            },
+            {
+                handle: `@StackOverflower ☣️`,
+                profilePic: `images/overflow.png`,
+                replyText: `No. Go.`,
+            }
+        ],
         isLiked: false
     }
 ]
